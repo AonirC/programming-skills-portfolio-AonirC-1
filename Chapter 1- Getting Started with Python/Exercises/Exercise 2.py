4b4928c5f28b6f7d0c8d5891b82432fa8f580dd0
AonirC <144213680+AonirC@users.noreply.github.com>
2023-10-07 19:07:46 +0400

    Exercise 2.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 2.py
@@ -0,0 +1,6 @@
+import sys
+print ("3.5.2 (default, Sep 10 2016, 08:21:44" )                                                                        
+["GCC 5.4.0 20160609" ] 
+print (sys.version)
+print ("Version info.")
+sys.version("major=3, minor=5, micro=2, releaselevel='final', serial=0" )
--- Chapter 1- Getting Started with Python/Practice 1.py
+++ Chapter 1- Getting Started with Python/Practice 1.py
@@ -1,11 +1,3 @@
-print ("abc" , "xyz" , 12345)
-print (13/2)
-print (13//2)
-print (15/5)
-print (15//5)
-print (22+2)
-#type cast float to int
-print ((2*3/8+(23+5)))
-print (2*3/8+\
-        ((23+5)))
-
+letter= "WXYZ"
+for index,letter in enumerate(letter):
+    print(index,":",letter)
