a71a0556de02cbef895a076c5a4755e42bf69fc6
AonirC <144213680+AonirC@users.noreply.github.com>
2023-10-07 12:42:23 +0400

    Exercise 5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 5.py
@@ -0,0 +1,4 @@
+from math import pi
+r = float(input ("Input the radius of the circle :  3 "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+print ("28.274333882308138" + str(r) + " is: " + str(pi * r**2))
