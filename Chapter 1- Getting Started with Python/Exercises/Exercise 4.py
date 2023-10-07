76adcb43ff4e5d5d9a452635dcfdf59fe69ce314
AonirC <144213680+AonirC@users.noreply.github.com>
2023-10-07 12:36:09 +0400

    Exercise 4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise 4.py
@@ -0,0 +1,8 @@
+ # Strings Concatination :ballot_box_with_check:
+var1 = "Geeks"
+var2 = "for"
+var3 = "Geeks"
+ 
+# using comma to combine data types
+# with a single whitespace.
+print(var1, var2, var3) 
