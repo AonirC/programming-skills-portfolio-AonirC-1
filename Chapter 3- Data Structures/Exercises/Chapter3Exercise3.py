f385903ff203ec9887db9f47a50ba71ebcffd097
AonirC <144213680+AonirC@users.noreply.github.com>
2023-10-24 21:33:59 +0400

    Chapter3Exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/Chapter3Exercise3.py
@@ -0,0 +1,4 @@
+favoritetransportation = ['motorcycle.', "private jet."]
+
+print("I would like to own a {}".format(favoritetransportation[0]))
+print("I would like to own a {}".format(favoritetransportation[1]))
