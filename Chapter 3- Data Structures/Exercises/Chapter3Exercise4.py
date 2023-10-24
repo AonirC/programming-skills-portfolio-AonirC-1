029345df49965c8e20e6f5be1c34c9d742110a63
AonirC <144213680+AonirC@users.noreply.github.com>
2023-10-24 21:36:38 +0400

    Chapter3Exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/Chapter3Exercise4.py
@@ -0,0 +1,10 @@
+guests = ['Hanna Crisna', 'Mae Gallego', 'Darnelle Infantes']
+
+name = guests[0].title()
+print(f"{name}, please come to the party.")
+
+name = guests[1].title()
+print(f"{name}, please come to the party.")
+
+name = guests[2].title()
+print(f"{name}, please come to the party.")
