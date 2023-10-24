61b31751027fb8d6027cfc2a7c077684427c7e6f
AonirC <144213680+AonirC@users.noreply.github.com>
2023-10-24 21:25:00 +0400

    Chapter2Exercise5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Chapter2Exercise5.py
@@ -0,0 +1,7 @@
+moneytotal = (50)
+flashdrive = (6)
+total_flashdrive = (50//6)
+totalchange = (50%6)
+
+print("The total number of flash drives she can buy is", total_flashdrive)
+print("She will have a total of £",totalchange, "left")
