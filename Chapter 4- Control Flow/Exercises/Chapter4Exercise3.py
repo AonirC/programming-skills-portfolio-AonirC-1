5c7b4995a9ecb4ad3ec0b3f18b6c223aa986a79f
AonirC <144213680+AonirC@users.noreply.github.com>
2023-11-18 11:05:26 +0400

    Chapter4Exercise3.py

---
+++ Chapter 4- Control Flow/Exercises/Chapter4Exercise3.py
@@ -0,0 +1,15 @@
+#if the alien_color is green, print a message that the player earned 5 points
+alien_color = "green"
+
+if alien_color == "green":
+    print("You got 5 points!")
+
+#if the alien_color is yellow, print a message that the player earned 10 points
+alien_color = "yellow"
+if alien_color == "yellow":
+    print("You got 10 points!")
+
+#if the alien_color is red, print a message that the player earned 15 points
+alien_color = 'red'
+if alien_color == "red":
+    print("You got 15 points!")
