94e9344f35fe169a72e2897269aa5c2eafddc28f
AonirC <144213680+AonirC@users.noreply.github.com>
2024-03-30 12:20:46 +0400

    Create Chapter4exercise5.py

---
+++ Chapter 4- Control Flow/Chapter4exercise5.py
@@ -0,0 +1,13 @@
+favorite_fruits = ['apples', 'pomelo', 'oranges', 'dragonfruit', 'cherries']
+
+#5 Statements if the fruit is in the list
+if 'apples' in favorite_fruits:
+    print("You really like apples!")
+if 'pomelo' in favorite_fruits: 
+    print("You really like pomelo!")
+if 'oranges' in favorite_fruits:
+    print("You reaally like oranges!")
+if 'dragonfruit' in favorite_fruits:
+    print("You really like dragonfruit!")
+if 'cherries' in favorite_fruits:
+    print("You really like cherries!")
