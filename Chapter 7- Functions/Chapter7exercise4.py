43661524a3724ebc7a89c3b9935eaccb257726bf
AonirC <144213680+AonirC@users.noreply.github.com>
2024-03-30 12:14:22 +0400

    Create Chapter7exercise4.py

---
+++ Chapter 7- Functions/Chapter7exercise4.py
@@ -0,0 +1,8 @@
+def make_shirt(size='large', quote='Python Rules!'):
+    """Summary of the shirt that's going to be made."""
+    print("\nThis is a " + size + " shirt.")
+    print('It says, "' + quote + '"')
+
+make_shirt()
+make_shirt(size='medium')
+make_shirt('small', 'Coding is cool.')
