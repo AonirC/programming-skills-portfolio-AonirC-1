47f1cde8dd922b3bfe21402b189dcce97418baf9
AonirC <144213680+AonirC@users.noreply.github.com>
2024-03-30 12:12:18 +0400

    Create Chapter7exercise1.py

---
+++ Chapter 7- Functions/Chapter7exercise1.py
@@ -0,0 +1,7 @@
+#display a message
+def display_message():
+    """Display a message about what I'm learning."""
+    message = "I'm learning on storing code in functions."
+    print(message)
+
+display_message()
